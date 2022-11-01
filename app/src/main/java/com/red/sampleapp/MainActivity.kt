4f559b8ca27810be087a9f61cb1c7822d72098d2
Ravil Khabiakhmetov <ravikwow@gmail.com>
2022-11-01 17:03:12 +0500

    [add] RandomFragment: deeplink navigation to about screen

--- app/src/main/java/com/red/sampleapp/MainActivity.kt
+++ app/src/main/java/com/red/sampleapp/MainActivity.kt
@@ -11,17 +11,13 @@ import androidx.navigation.ui.setupWithNavController
 import com.red.base.ui.activity.ViewBindingActivity
 import com.red.sampleapp.databinding.ActivityMainBinding
 import com.red.sampleapp.feature.aboutmovie.MovieAboutUI
-import com.red.sampleapp.feature.random.OnRandomFragmentListener
-import com.red.sampleapp.feature.random.RandomFragment
-import com.red.sampleapp.feature.random.RandomFragmentDirections
 import com.red.sampleapp.feature.saved.OnSavedFragmentListener
 import com.red.sampleapp.feature.saved.SavedFragment
 import com.red.sampleapp.feature.saved.SavedFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ViewBindingActivity<ActivityMainBinding>(),
-    OnRandomFragmentListener, OnSavedFragmentListener {
+class MainActivity : ViewBindingActivity<ActivityMainBinding>(), OnSavedFragmentListener {
     lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -42,16 +38,6 @@ class MainActivity : ViewBindingActivity<ActivityMainBinding>(),
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration)
-    }
-
-    override fun randomBtnClick(fragment: RandomFragment) {
-        val action = RandomFragmentDirections.actionRandomScreenmToAboutScreenRandom(
-            MovieAboutUI(
-                -1,
-                getString(com.red.sampleapp.feature.random.R.string.title_random)
-            )
-        )
-        findNavController(fragment).navigate(action)
     }
 
     override fun savedBtnClick(fragment: SavedFragment) {
--- feature/random/src/main/java/com/red/sampleapp/feature/random/RandomFragment.kt
+++ feature/random/src/main/java/com/red/sampleapp/feature/random/RandomFragment.kt
@@ -1,30 +1,31 @@
 package com.red.sampleapp.feature.random
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.net.toUri
+import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.navOptions
 import com.red.base.ui.fragment.ViewBindingFragment
 import com.red.sampleapp.feature.random.databinding.FragmentRandomBinding
 
 class RandomFragment : ViewBindingFragment<FragmentRandomBinding>() {
-    private lateinit var parentCallback: OnRandomFragmentListener
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btn.setOnClickListener {
-            parentCallback.randomBtnClick(this)
+            val id = -1
+            val name = getString(R.string.title_random)
+            val request = NavDeepLinkRequest.Builder
+                .fromUri("android-app://com.red.sampleapp/aboutmovie/${id}/?name=${name}".toUri())
+                .build()
+            NavHostFragment.findNavController(this).navigate(request, navOptions {
+                anim {
+                    enter = android.R.anim.fade_in
+                    exit = android.R.anim.fade_out
+                    popEnter = android.R.anim.fade_in
+                    popExit = android.R.anim.fade_out
+                }
+            })
         }
     }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        try {
-            parentCallback = context as OnRandomFragmentListener
-        } catch (e: ClassCastException) {
-            throw ClassCastException("$context must implement OnRandomFragmentListener")
-        }
-    }
-}
-
-interface OnRandomFragmentListener {
-    fun randomBtnClick(fragment: RandomFragment)
 }
